3a5055b7b14688277232712935a5c3608b63fcd7
Mohil Parmar <145832606+Mohil0003@users.noreply.github.com>
2025-05-12 20:30:42 +0530

    push from vs code

---
+++ src/mohil.js
@@ -0,0 +1,11 @@
+function Mohil(){
+    return(
+        <div>
+            <h1>Mohil</h1>
+            <p>Mohil is a software engineer.</p>
+        </div>
+    )
+}
+
+export default Mohil;
+// This code defines a React functional component named "Mohil".
